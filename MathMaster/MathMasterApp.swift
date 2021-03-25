165bd19ebbd695464b781185fa4168c88b8d0c91
Michael Lekrans <mlekrans@hotmail.com>
2021-03-25 07:49:24 +0100

    Initial Commit

---
+++ MathMaster/MathMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasterApp.swift
+//  MathMaster
+//
+//  Created by Michael Lekrans on 2021-03-25.
+//
+
+import SwiftUI
+
+@main
+struct MathMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
